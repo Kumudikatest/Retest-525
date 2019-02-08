2b295af58476fcbb315c39bef79a80ce41fc93ea
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-02-08 16:29:05 +0530

    test

--- Retest-525/lambda.js
+++ Retest-525/lambda.js
@@ -1,4 +1,28 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+    sns.listSubscriptionsByTopic({
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:dynamodb'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+    cognito_idp.listUsers({
+        UserPoolId: "us-east-1_D10y3fy0o",
+        Limit: "10"
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
+
+
+    callback(null, { "message": "Successfully executed" });
 }
